10e4b2a50c8f22642d1241927e777cbaa1ac0c91
Atoengg <kromosom06@gmail.com>
2023-07-06 20:32:43 +0700

    add aos animation

--- asets/js/main.js
+++ asets/js/main.js
@@ -1,46 +1,46 @@
 // animasi hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function () {
-   hamburger.classList.toggle('hamburger-active'); 
-   navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // navbar fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    const toTop = document.querySelector('#to-top');
-  
-    if(window.pageYOffset > fixedNav){
-      header.classList.add('navbar-fixed');
-      toTop.classList.remove('hidden');
-      toTop.classList.add('flex');
-    } else{
-      header.classList.remove('navbar-fixed');
-      toTop.classList.remove('flex');
-      toTop.classList.add('hidden');
-    }
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
 
 // klik di luar hamburger
-window.addEventListener('click', function (e) {
+window.addEventListener("click", function (e) {
   if (e.target != hamburger && e.target != navMenu) {
-    hamburger.classList.remove('hamburger-active'); 
-    navMenu.classList.add('hidden');
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
   }
 });
 
 // dark mode
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function () {
+darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
-    html.classList.add('dark');
-  } else{
-    html.classList.remove('dark');
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
   }
 });
